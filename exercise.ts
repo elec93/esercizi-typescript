0591c82a17c20c6832d47711d8fec4c4b8210b85
Eleonora <eleonoracasto93@gmail.com>
2023-03-29 13:30:07 +0200

    obj es 3

--- exercise.ts
+++ exercise.ts
@@ -1,45 +1,33 @@
-// Change the `Country` interface so the `population` property is optional.
-// This will fix the type error for `countryB`.
-
 interface Country {
-  name: string;
-  code: string;
-  population?: number;
+    name: string;
+    code: string;
 }
 
-const countryA: Country = {
-  name: "India",
-  code: "IN",
-  population: 1_352_642_280,
+// Complete the `CountryWithPopulation` interface:
+//
+// - Extend the `Country` interface
+// - Add a `population` property with an appropriate type
+
+interface CountryWithPopulation extends Country{
+    population: number,
+}
+
+const countryA: CountryWithPopulation = {
+    name: "India",
+    code: "IN",
+    population: 1_352_642_280,
 };
 
 const countryB: Country = {
-  name: "Italy",
-  code: "IT",
+    name: "Italy",
+    code: "IT"
 };
 
-const countryC: Country = {
-  name: "Spain",
-  code: "ES",
-  population: 47_450_795,
+const countryC: CountryWithPopulation = {
+    name: "Spain",
+    code: "ES",
+    population: 47_450_795,
 };
-
-// Change the `Currency` type alias so the `name` property cannot be changed.
-// This will cause a type error. Remove the code that now has a type error.
-
-type Currency = {
-  readonly name: string;
-  code: string;
-  symbol: string;
-};
-
-const currencyData: Currency = {
-  name: "Euro",
-  code: "EUR",
-  symbol: "€",
-};
-
-currencyData.code = "GGG";
 
 // ----
 
