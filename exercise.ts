c06d7049a39dbc4fd667b5edce549456bc083867
Eleonora <eleonoracasto93@gmail.com>
2023-04-01 12:22:27 +0200

    construct es 2

--- exercise.ts
+++ exercise.ts
@@ -2,77 +2,40 @@
  * START: Follow the instructions below.
  */
 
-interface Country {
-  name: string;
-  code: string;
-  languages: string[];
-  currency?: string;
-  population: number;
+// Fix this use of the `Record` utility type so it constructs a valid type.
+
+const currency: Record<string, string> = {
+    name: "United States dollar",
+    code: "USD",
+    symbol: "$",
+};
+
+// Add a type annotation to the `currencies` variable that uses
+// the `Record` utility type.
+// Hint: Pass the `Currency` type as the second type argument to `Record<>`.
+
+interface Currency {
+    name: string;
+    code: string;
+    symbol: string;
 }
 
-// Change the `PartialCountry` type to use the `Partial` utility type.
-// This should fix the type error for the `countryA` object.
-
-type PartialCountry = Country;
-
-const countryA: Partial<PartialCountry> = {
-  code: "CN",
-  population: 1_412_600_000,
-};
-
-// Change the `CompleteCountry` type to use the `Required` utility type.
-// This should cause a type error for the `countryB` object.
-// Fix the type error by adding the missing property to the object.
-
-type CompleteCountry = Country;
-
-const countryB: Required<CompleteCountry> = {
-  name: "Greece",
-  code: "GR",
-  languages: ["Greek"],
-  population: 10_678_632,
-  currency: "Atene",
-};
-
-// Change the `ReadonlyCountry` type to use the `Readonly` utility type.
-// This should cause a type error in the code below.
-// Remove the code that is incorrect.
-
-type ReadonlyCountry = Country;
-
-const countryC: Readonly<ReadonlyCountry> = {
-  name: "Italy",
-  code: "IT",
-  languages: ["Italian"],
-  population: 60_317_116,
-};
-
-// countryC.population = 60_317_117;
-
-console.log(countryC);
-
-// Change the `CountryWithPopulation` type to use the `Pick` utility type.
-// Hint: The syntax is: Pick<Type, Keys>
-// This should fix the type error for the `countryD` object.
-
-type CountryWithPopulation = Pick<Country, "name" | "code" | "population">;
-
-const countryD: CountryWithPopulation = {
-  name: "New Zealand",
-  code: "NZ",
-  population: 5_135_300,
-};
-
-// Change the `CountryWithoutPopulation` type to use the `Omit` utility type.
-// This should fix the type error for the `countryE` object.
-
-type CountryWithoutPopulation = Omit<Country, 'population'>;
-
-const countryE: CountryWithoutPopulation = {
-  name: "Thailand",
-  code: "TH",
-  languages: ["Thai", "Isan", "Kam Mueang", "Pak Tai", "Malay"],
-  currency: "Baht",
+const currencies: Record<string, Currency> = {
+    france: {
+        name: "Euro",
+        code: "EUR",
+        symbol: "€",
+    },
+    china: {
+        name: "Renminbi",
+        code: "CNY",
+        symbol: "¥",
+    },
+    colombia: {
+        name: "Colombian peso",
+        code: "COP",
+        symbol: "$",
+    }
 };
 
 // ----
