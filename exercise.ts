16825e13a370a1e75a5f014aa7972a3a2f10df52
Eleonora <eleonoracasto93@gmail.com>
2023-04-01 12:17:21 +0200

    construct es 1

--- exercise.ts
+++ exercise.ts
@@ -2,27 +2,77 @@
  * START: Follow the instructions below.
  */
 
- type CountryData = {
-    name: string;
-    languages: string[];
-    population: number;
+interface Country {
+  name: string;
+  code: string;
+  languages: string[];
+  currency?: string;
+  population: number;
+}
+
+// Change the `PartialCountry` type to use the `Partial` utility type.
+// This should fix the type error for the `countryA` object.
+
+type PartialCountry = Country;
+
+const countryA: Partial<PartialCountry> = {
+  code: "CN",
+  population: 1_412_600_000,
 };
 
-// Complete the syntax for this mapped type.
-// Hint: Mapped types are generic.
-//
-// Replace the `never` type with the correct type.
-//
-// These changes should fix all the type errors.
+// Change the `CompleteCountry` type to use the `Required` utility type.
+// This should cause a type error for the `countryB` object.
+// Fix the type error by adding the missing property to the object.
 
-type Descriptions<Type> = {
-    [Property in keyof Type]: string;
+type CompleteCountry = Country;
+
+const countryB: Required<CompleteCountry> = {
+  name: "Greece",
+  code: "GR",
+  languages: ["Greek"],
+  population: 10_678_632,
+  currency: "Atene",
 };
 
-const countryDataDescriptions: Descriptions<CountryData> = {
-    name: "The name of the country.",
-    languages: "The languages spoken in the country.",
-    population: "The population of the country.",
+// Change the `ReadonlyCountry` type to use the `Readonly` utility type.
+// This should cause a type error in the code below.
+// Remove the code that is incorrect.
+
+type ReadonlyCountry = Country;
+
+const countryC: Readonly<ReadonlyCountry> = {
+  name: "Italy",
+  code: "IT",
+  languages: ["Italian"],
+  population: 60_317_116,
+};
+
+// countryC.population = 60_317_117;
+
+console.log(countryC);
+
+// Change the `CountryWithPopulation` type to use the `Pick` utility type.
+// Hint: The syntax is: Pick<Type, Keys>
+// This should fix the type error for the `countryD` object.
+
+type CountryWithPopulation = Pick<Country, "name" | "code" | "population">;
+
+const countryD: CountryWithPopulation = {
+  name: "New Zealand",
+  code: "NZ",
+  population: 5_135_300,
+};
+
+// Change the `CountryWithoutPopulation` type to use the `Omit` utility type.
+// This should fix the type error for the `countryE` object.
+
+type CountryWithoutPopulation = Omit<Country, 'population'>;
+
+const countryE: CountryWithoutPopulation = {
+  name: "Thailand",
+  code: "TH",
+  languages: ["Thai", "Isan", "Kam Mueang", "Pak Tai", "Malay"],
+  currency: "Baht",
 };
 
 // ----
